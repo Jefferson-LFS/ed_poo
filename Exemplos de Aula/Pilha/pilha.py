7085440cd18e61421d538839a20d13053147c0ea
Alana MM <alanamm.prof@gmail.com>
2020-09-13 16:02:43 -0300

    update

--- Exemplos de Aula/Pilha/pilha.py
+++ Exemplos de Aula/Pilha/pilha.py
@@ -2,17 +2,29 @@ class Pilha:
     def __init__(self):
         self.__dados = []
 
-    def getPilha(self):
-        return self.__dados
+    #retorna True se for vazia
+    def is_empty(self):
+      return len(self.__dados) == 0
 
     def push(self, novoElem):
         self.__dados.append(novoElem)
 
     def pop(self):
-        self.__dados.pop()
+        if self.is_empty():
+            return "Lista Vazia - Não houve Remo"
+        return self.__dados.pop()
+
+    def __str__(self):
+        return str(self.__dados)
 
     def topo(self):
-        return (self.__dados[len(self.__dados) - 1])
+      if self.is_empty():
+        return ("Pilha vazia")
+      else: 
+        return self.__dados[-1]
+
+    def __len__(self): 
+      return len(self.__dados)
 
     def esvaziar(self):
         while (len(self.__dados) != 0):
--- Exemplos de Aula/Pilha/teste.py
+++ Exemplos de Aula/Pilha/teste.py
@@ -1,21 +1,20 @@
 from pilha import Pilha
 
 def main():
-
     pilhaTeste = Pilha()
     pilhaTeste.push("Teste")
     pilhaTeste.push("Teste2")
-    print(pilhaTeste.getPilha())
+    print(pilhaTeste)
     pilhaTeste.pop()
-    print(pilhaTeste.getPilha())
+    print(pilhaTeste)
 
     nome = input("Digite o nome do usuario completo: ")
     for n in nome.split(" "):
         pilhaTeste.push(n.strip())
 
-    print(pilhaTeste.getPilha())
+    print(pilhaTeste)
     pilhaTeste.pop()
-    print(pilhaTeste.getPilha())
+    print(pilhaTeste)
     print(pilhaTeste.topo())
 
     pilhaTeste.esvaziar()
